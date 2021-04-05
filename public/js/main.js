f662ba52076af3855c19f982401bdc2d5195d16e
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-04 21:23:10 -0700

    added page scroller. credit to Edvinas Pranka @ dev.to

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,50 @@
-const log = console.log;
+$(document).ready(() => {
+    const sections = $(".section");
+    let currentIndex = 0;
+    let isAnimating = false;
+
+    const stopAnimation = () => {
+        setTimeout(() => {
+            isAnimating = false;
+        }, 400);
+    };
+
+    document.addEventListener(
+        "wheel",
+        (event) => {
+            if (isAnimating) {
+                event.preventDefault();
+                return;
+            }
+
+            const direction = event.deltaY;
+
+            if (direction > 0) {
+                if (currentIndex + 1 >= sections.length) return;
+                currentIndex++;
+                const nextSection = $(sections[currentIndex]);
+                var offsetTop = nextSection.offset().top;
+                event.preventDefault();
+                isAnimating = true;
+                $("html, body").animate(
+                    { scrollTop: offsetTop },
+                    250,
+                    stopAnimation
+                );
+            } else {
+                if (currentIndex - 1 < 0) return;
+                currentIndex--;
+                const previousSection = $(sections[currentIndex]);
+                const offsetTop = previousSection.offset().top;
+                event.preventDefault();
+                isAnimating = true;
+                $("html, body").animate(
+                    { scrollTop: offsetTop },
+                    250,
+                    stopAnimation
+                );
+            }
+        },
+        { passive: false }
+    );
+});
