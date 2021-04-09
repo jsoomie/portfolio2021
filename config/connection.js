7efebe30ba77158fd57995a683e11c2e889cdb24
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-09 13:49:01 -0700

    removed unused connection config folder/file

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const db_name = "";
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${db_name}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
